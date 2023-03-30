ac9393812ff46c3da7aec76c1661941d75bafce2
OLING <yealin9399@gmail.com>
2023-03-30 21:16:02 +0900

    feat: 3번째 뷰 수정

--- narangnorang_ios.swiftpm/SceneFirstView.swift
+++ narangnorang_ios.swiftpm/SceneFirstView.swift
@@ -1,5 +1,5 @@
 //
-//  SceneThreeView.swift
+//  SceneFirstView.swift
 //  narangnorang_ios
 //
 //  Created by OLING on 2023/03/30.
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-struct SceneThreeView: View {
+struct SceneFirstView: View {
         var body: some View {
             ZStack{
-                Image("LEEO(01)")
+                Image("main")
                     .resizable()
-                    .frame(width: 1200, height: 900)
-                    .ignoresSafeArea()
+                    .frame(width: 1250, height: 850)
+                   
             }
 
     }
 }
 
-struct SceneThreeView_Previews: PreviewProvider{
+struct SceneFirstView_Previews: PreviewProvider{
     static var previews: some View {
-        SceneThreeView()
+        SceneFirstView()
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
+
